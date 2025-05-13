1b4000441d01a7ae67fd0b6acbd8a178ede36246
Joerbeth95 <82187575+Joerbeth95@users.noreply.github.com>
2025-05-13 16:37:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import requests
 import json
 import time
-import random 
+import random
+import os  # <- Necessário para acessar as variáveis de ambiente
 
-# Register the azure app first and make sure the app has the following permissions:
-# files: Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
-# user: User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
-# mail: Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
-# After registration, you must click on behalf of xxx to grant administrator consent, otherwise outlook api cannot be called
-
-
-
-
-
-
+# Lista de chamadas para APIs da Microsoft
 calls = [
     'https://graph.microsoft.com/v1.0/me/drive/root',
     'https://graph.microsoft.com/v1.0/me/drive',
@@ -35,7 +26,6 @@ calls = [
     'https://graph.microsoft.com/v1.0/sites/root/drives'
 ]
 
-
 def get_access_token(refresh_token, client_id, client_secret):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -47,34 +37,54 @@ def get_access_token(refresh_token, client_id, client_secret):
         'client_secret': client_secret,
         'redirect_uri': 'http://localhost:53682/'
     }
-    html = requests.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
-    jsontxt = json.loads(html.text)
-    refresh_token = jsontxt['refresh_token']
-    access_token = jsontxt['access_token']
-    return access_token
+    response = requests.post(
+        'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+        data=data,
+        headers=headers
+    )
+    json_data = response.json()
+    
+    if 'access_token' not in json_data:
+        raise Exception(f"Erro ao obter token: {json_data}")
+    
+    # Se você quiser atualizar o token de refresh, pegue aqui também
+    return json_data['access_token']
 
 def main():
-    random.shuffle(calls)
-    endpoints = calls[random.randint(0,10)::]
+    # Pega variáveis do ambiente (fornecidas no GitHub Actions)
+    refresh_token = os.environ["REFRESH_TOKEN"]
+    client_id = os.environ["CONFIG_ID"]
+    client_secret = os.environ["CONFIG_KEY"]
+
+    # Obtém access token
     access_token = get_access_token(refresh_token, client_id, client_secret)
+
+    # Define cabeçalhos e embaralha chamadas
     session = requests.Session()
     session.headers.update({
-        'Authorization': access_token,
+        'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
     })
+
+    random.shuffle(calls)
+    endpoints = calls[random.randint(0, 10):]
     num = 0
+
     for endpoint in endpoints:
         try:
             response = session.get(endpoint)
             if response.status_code == 200:
                 num += 1
-                print(f'{num}th Call successful')
+                print(f'{num}ª chamada bem-sucedida: {endpoint}')
+            else:
+                print(f'Erro {response.status_code} ao chamar: {endpoint}')
         except requests.exceptions.RequestException as e:
-            print(e)
-            pass
-    localtime = time.asctime(time.localtime(time.time()))
-    print('The end of this run is :', localtime)
-    print('Number of calls is :', str(len(endpoints)))
+            print(f"Erro na requisição: {e}")
 
+    localtime = time.asctime(time.localtime(time.time()))
+    print('Execução concluída em:', localtime)
+    print('Total de chamadas realizadas:', str(num))
+
+# Executa 3 vezes
 for _ in range(3):
     main()
